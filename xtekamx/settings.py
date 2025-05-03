8e38a28ece0cf34849c70476a19dae77d352ab58
matavaresr <matr192001@hotmail.com>
2025-05-03 12:03:37 -0600

    retry of deploy

--- xtekamx/settings.py
+++ xtekamx/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
