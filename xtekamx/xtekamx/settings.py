d4cdec373839758a7343713aaddbc93dd3d02c8d
matavaresr <matr192001@hotmail.com>
2025-05-03 07:45:30 -0600

    add como llegar, acerca de & part of paquete

--- xtekamx/xtekamx/settings.py
+++ xtekamx/xtekamx/settings.py
@@ -26,6 +26,8 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
 
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+
 # Application definition
 
 INSTALLED_APPS = [
